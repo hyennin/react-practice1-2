7ddd6dac5217665cff81d279b444fa27bf565546
hyennin <chm050202@gmail.com>
2023-04-17 14:01:31 +0900

    useReducer 실습

--- src/App.js
+++ src/App.js
@@ -4,15 +4,8 @@ import Info from "./Info";
 import Timer from "./Timer";
 
 const App = () => {
-  const [showTimer, setShowTimer] = useState(false);
-
   return (
-    <div>
-      {showTimer && <Timer/>}
-      <button onClick={() => setShowTimer(!showTimer)}>
-        Toggle
-      </button>
-    </div>
+    <Counter/>
   );
 };
 
