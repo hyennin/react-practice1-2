1c7d0ed8f315a6a26690fe8152a175e2404ea235
hyennin <chm050202@gmail.com>
2023-04-24 14:06:10 +0900

    실습

---
+++ src/App copy.js
@@ -0,0 +1,52 @@
+import Counter from "./Counter";
+import { useMemo, useState } from "react";
+import Info from "./Info";
+import Timer from "./Timer";
+
+const hardCalculate = (number) => {
+  console.log('시간이 많이 걸리는 계산');
+  for (let i = 0; i < 999999999; i++) { }
+
+  return number + 10000;
+}
+
+const easyCalculate = (number) => {
+  console.log('시간이 적게 걸리는 계산');
+
+  return number + 1;
+}
+
+const App = () => {
+  const [hardNumber, setHardNumber] = useState(1);
+  const [easyNumber, setEasyNumber] = useState(1);
+
+  // const hardSum = hardCalculate(hardNumber);
+  const hardSum = useMemo(() => {
+    // 메모어제이션을 하게 되면 처음 렌더링 할 때만 실행되어 그 값을 메모리에 저장
+    return hardCalculate(hardNumber);
+  }, [hardNumber]);
+
+  const easySum = easyCalculate(easyNumber);
+
+  return (
+    <div>
+      <h3>시간이 많이 걸리는 계산기</h3>
+      <input
+        type="number"
+        value={hardNumber}
+        onChange={(e) => setHardNumber(parseInt(e.target.value))}
+      />
+      <span>+10000 = {hardSum}</span>
+
+      <h3>시간이 적게 걸리는 계산기</h3>
+      <input
+        type="number"
+        value={easyNumber}
+        onChange={(e) => setEasyNumber(parseInt(e.target.value))}
+      />
+      <span>+10000 = {easySum}</span>
+    </div>
+  );
+};
+
+export default App;
--- src/App.js
+++ src/App.js
@@ -1,52 +1,28 @@
-import Counter from "./Counter";
-import { useMemo, useState } from "react";
-import Info from "./Info";
-import Timer from "./Timer";
-
-const hardCalculate = (number) => {
-  console.log('시간이 많이 걸리는 계산');
-  for (let i = 0; i < 999999999; i++) { }
-
-  return number + 10000;
-}
-
-const easyCalculate = (number) => {
-  console.log('시간이 적게 걸리는 계산');
-
-  return number + 1;
-}
+import React, { useEffect, useState } from "react";
 
 const App = () => {
-  const [hardNumber, setHardNumber] = useState(1);
-  const [easyNumber, setEasyNumber] = useState(1);
+  const [number, setNumber] = useState(0);
+  const [isKorea, setIsKorea] = useState(true);
 
-  // const hardSum = hardCalculate(hardNumber);
-  const hardSum = useMemo(() => {
-    // 메모어제이션을 하게 되면 처음 렌더링 할 때만 실행되어 그 값을 메모리에 저장
-    return hardCalculate(hardNumber);
-  }, [hardNumber]);
-
-  const easySum = easyCalculate(easyNumber);
+  const location = isKorea ? "한국" : "외국";
+  
+  useEffect(() => {
+    console.log('useEffect 호출');
+  }, [location]);
 
   return (
     <div>
-      <h3>시간이 많이 걸리는 계산기</h3>
+      <h2>하루 몇 끼 먹어요?</h2>
       <input
         type="number"
-        value={hardNumber}
-        onChange={(e) => setHardNumber(parseInt(e.target.value))}
-      />
-      <span>+10000 = {hardSum}</span>
-
-      <h3>시간이 적게 걸리는 계산기</h3>
-      <input
-        type="number"
-        value={easyNumber}
-        onChange={(e) => setEasyNumber(parseInt(e.target.value))}
-      />
-      <span>+10000 = {easySum}</span>
+        value={number}
+        onChange={(e) => setNumber(e.target.value)}/>
+        <hr/>
+        <h2>어느 나라에 있어요?</h2>
+        <p>나라: {location}</p>
+        <button onClick={() => setIsKorea(!isKorea)}>비행기 타자</button>                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           
     </div>
-  );
-};
+  )
+}
 
 export default App;
