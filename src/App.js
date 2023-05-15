e8c42649aab657e61c51795ae968fe111286b576
Hyemin Choi <90095211+hyennin@users.noreply.github.com>
2023-05-15 15:17:27 +0900

    useInput 실습

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
+// App.js
 import { useInput } from "./useInput";
 
+function displayMessage(message) {
+  alert(message);
+}
 const App = () => {
-  const [inputValue, handleChange] = useInput("안녕");
-  const handleSubmit = () => {
-    alert(inputValue);
-    //setInputValue("");
-  };
+  const [inputValue, handleChange, handleSubmit] = useInput("", displayMessage);
+  
   return (
     <div>
-    <h1>useInput</h1>
-    <input value={inputValue} onChange={handleChange} />
-    <button onClick={handleSubmit}>확인</button>
+      <h1>useInput</h1>
+      <input value={inputValue} onChange={handleChange} />
+      <button onClick={handleSubmit}>확인</button>
     </div>
   );
 };
--- src/useInput.js
+++ src/useInput.js
@@ -1,12 +1,13 @@
 import { useState } from "react";
-export function useInput(initialValue) {
+
+export function useInput(initialValue, submitAction) {
     const [inputValue, setInputValue] = useState(initialValue);
     const handleChange = (e) => {
         setInputValue(e.target.value);
     };
     const handleSubmit = () => {
         setInputValue("");
-        alert(inputValue);
+        submitAction(inputValue);
     };
     return [inputValue, handleChange, handleSubmit];
 }
